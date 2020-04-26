9ddd1b0e5b0677b6dfd52d73122bd9d94ee74774
tushar sahay <tsahay2@gmail.com>
2020-04-27 01:36:55 +0530

    Updates

---
+++ precache-manifest.d5f6778fd7d819cdc14a75dba63e14f1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9b25ce75c566fe7f0ca8",
+    "url": "/full-portfolio/static/css/main.9972f7f8.chunk.css"
+  },
+  {
+    "revision": "9b25ce75c566fe7f0ca8",
+    "url": "/full-portfolio/static/js/main.9b25ce75.chunk.js"
+  },
+  {
+    "revision": "bb6ae70a59d000bc085d",
+    "url": "/full-portfolio/static/js/1.bb6ae70a.chunk.js"
+  },
+  {
+    "revision": "69bd31806d496027b41d",
+    "url": "/full-portfolio/static/js/runtime~main.69bd3180.js"
+  },
+  {
+    "revision": "fc387e8979daa93b82bf47205628cb18",
+    "url": "/full-portfolio/index.html"
+  }
+];
