622bda422f0fcb4828a58fd5ab0aac8e950d615d
tushar sahay <tsahay2@gmail.com>
2020-05-02 02:13:23 +0530

    Correcting typo in social media section

--- src/Components/SocialMedia.js
+++ src/Components/SocialMedia.js
@@ -19,7 +19,7 @@ render() {
                 <div className="row">
                     <div className="twelve columns collapsed">
 
-                        <h1>Check Out Some of proud tweets.</h1>
+                        <h1>Check out some of my proud tweets.</h1>
                     </div>
                     <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
                         <ul className="slides">
